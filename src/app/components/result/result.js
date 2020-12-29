690b8810f01dac6e3e3bb55110179261d5d27d3a
zoomi-raja <zoomi.raja@gmail.com>
2020-12-29 14:28:24 +0400

    loader component

--- src/app/components/result/result.js
+++ src/app/components/result/result.js
@@ -1,8 +1,10 @@
+import { Spinner } from '../ui/spinner';
 import { Gist } from './gist/gist';
 import './style.css';
 export function Result() {
   return (
     <div className='results'>
+      <Spinner />
       {[...Array(5)].map( () => (<Gist />))}
 
     </div>
