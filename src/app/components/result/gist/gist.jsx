f48b0f4a0cb3bd6f737771e4bf602d6df36349b4
zamurd <zoomi.raja@gmail.com>
2025-01-31 15:30:58 +0400

    fix: prop spread

--- src/app/components/result/gist/gist.jsx
+++ src/app/components/result/gist/gist.jsx
@@ -1,28 +1,35 @@
-import { dateToYMD } from '../../../utilities/date';
-import PropTypes from 'prop-types';
+import { dateToYMD } from "../../../utilities/date";
+import PropTypes from "prop-types";
 
-import './style.css';
-export function Gist({data}) {    
+import "./style.css";
+export function Gist({ data: { owner, html_url, description, created_at } }) {
   return (
-    <div className='gist'>
-      <div className='gist__user'>
-        <div className='user__avatar'>
-          <img src={data.owner.avatar_url} alt="git user"/>
+    <div className="gist">
+      <div className="gist__user">
+        <div className="user__avatar">
+          <img src={owner.avatar_url} alt="git user" />
         </div>
-        <div className='user__info'>
-          <a className='user__name' href={data.owner.html_url} target="_blank" rel="noopener noreferrer">
-            <span className='user__name--limit' >{data.owner.login}</span>
+        <div className="user__info">
+          <a
+            className="user__name"
+            href={owner.html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="user__name--limit">{owner.login}</span>
             <span>&rarr;</span>
           </a>
-          <span>{dateToYMD(new Date(data.created_at))}</span>
+          <span>{dateToYMD(new Date(created_at))}</span>
         </div>
-			</div>
-      <div className='gist__desc'>
-        <a href={data.html_url} target="_blank" rel="noopener noreferrer">{data.description?data.description:'No Discription Available'}</a>
+      </div>
+      <div className="gist__desc">
+        <a href={html_url} target="_blank" rel="noopener noreferrer">
+          {description ? description : "No Discription Available"}
+        </a>
       </div>
     </div>
-  )
+  );
 }
 Gist.propTypes = {
-  data: PropTypes.object
+  data: PropTypes.object,
 };
